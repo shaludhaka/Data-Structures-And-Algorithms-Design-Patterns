50b286ed1d80ad998e973ed55df0daee3adece80
shalu <shalu.dhaka@cointribe.com>
2019-07-14 22:00:32 +0530

    deletion in a bst

--- algorithms/src/main/java/com/datastructures/Trees/Bst.java
+++ algorithms/src/main/java/com/datastructures/Trees/Bst.java
@@ -22,6 +22,7 @@ public class Bst {
         root = insertRecord(root, key);
     }
 
+
     //Insert into BST
     Node insertRecord(Node root, int rec){
 
@@ -39,10 +40,10 @@ public class Bst {
     }
 
     //inorder to call actual traversal function as we can not call class variable from static context
-
     void inorder(){
         inorderTraversal(root);
     }
+
 
     //inorder traversal of BST
     void inorderTraversal(Node root){
@@ -56,6 +57,64 @@ public class Bst {
     }
 
 
+    //search a key in BST
+    Node search(Node root, int key){
+
+        if (root==null || root.key == key){
+            return root;
+        }
+        else if (root.key > key){
+            return search(root.left, key);
+        }
+        else{
+            return search(root.right, key);
+        }
+    }
+
+
+    //call delete node function
+    void deleteValue(int key){
+        root = deleteValueItem(root, key);
+    }
+
+    //delete key
+    Node deleteValueItem(Node root, int key){
+        if (root == null){
+            return root;
+        }
+
+        if (key < root.key){
+            root.left = deleteValueItem(root.left, key);
+        }
+        else if (key > root.key){
+            root.right = deleteValueItem(root.right, key);
+        }
+        else{
+
+            if (root.left == null){
+                return  root.right;
+            }
+            else if (root.right == null){
+                return root.left;
+            }
+            else{
+                root.key = minInRightSubTree(root.right);
+                root.right = deleteValueItem(root.right, root.key);
+            }
+        }
+        return root;
+    }
+
+    //returns min value in a tree
+    int minInRightSubTree(Node root){
+        int min = root.key;
+        while(root.left!=null){
+            min = root.left.key;
+            root = root.left;
+        }
+        return min;
+    }
+
 
     public static void main(String[] args) {
          Bst bst = new Bst();
@@ -68,8 +127,12 @@ public class Bst {
          bst.insert(80);
 
          bst.inorder();
+         System.out.println("\nDelete 70");
+         bst.deleteValue(70);
+         bst.inorder();
     }
 }
+
 
 //Time complexity will be O(h)  h=height of BST
 //in case of skewed tree height will be n and so the search and insert will be O(n)
