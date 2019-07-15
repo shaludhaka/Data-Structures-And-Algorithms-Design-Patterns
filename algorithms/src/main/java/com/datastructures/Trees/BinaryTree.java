66a025955d8ed59a91723c9fb7980c0d89ca6ff4
shalu <shalu.dhaka@cointribe.com>
2019-07-15 16:43:09 +0530

    bt traversal changes

--- algorithms/src/main/java/com/datastructures/Trees/BinaryTree.java
+++ algorithms/src/main/java/com/datastructures/Trees/BinaryTree.java
@@ -28,8 +28,16 @@ public class BinaryTree {
 
         while(stack.empty()!=true){
             Node top = stack.peek();
-            System.out.println(top.key + " ");
+            System.out.print(top.key + " ");
+            stack.pop();
 
+            if (top.right!=null) {
+                stack.push(top.right);
+            }
+
+            if(top.left!=null){
+                stack.push(top.left);
+            }
         }
     }
 
@@ -37,10 +45,11 @@ public class BinaryTree {
     public static void main(String[] args) {
         BinaryTree  tree = new BinaryTree();
         tree.root = new Node(10);
-        tree.root.left = new Node(20);
-        tree.root.right = new Node(20);
-        tree.root.left.right = new Node(10);
-        tree.root.left.left = new Node(9);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(2);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(2);
 
         tree.preorderIterative();
     }
